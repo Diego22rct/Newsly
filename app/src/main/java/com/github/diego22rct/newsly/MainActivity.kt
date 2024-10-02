671c0e9ed451321c7f4aaa8df4135f0ddc459ffa
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-10-02 11:34:38 -0500

    added the navigation in the screens

--- app/src/main/java/com/github/diego22rct/newsly/MainActivity.kt
+++ app/src/main/java/com/github/diego22rct/newsly/MainActivity.kt
@@ -8,11 +8,28 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -23,8 +40,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.github.diego22rct.newsly.common.Screens
+import com.github.diego22rct.newsly.home.presentation.HomeScreen
+import com.github.diego22rct.newsly.news.presentation.FavoriteNewsScreen
+import com.github.diego22rct.newsly.news.presentation.SearchNewsScreen
 import com.github.diego22rct.newsly.ui.theme.NewslyTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,26 +54,69 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            val navController = rememberNavController()
             NewslyTheme {
-
+                val navController = rememberNavController()
+                Scaffold(
+                    bottomBar = {
+                        BottomAppBar(
+                            actions = {
+                                Row(
+                                    modifier = Modifier
+                                        .fillMaxHeight()
+                                        .fillMaxWidth()
+                                        .size(12.dp),
+                                    horizontalArrangement = Arrangement.Center,
+                                    verticalAlignment = Alignment.CenterVertically,
+                                ) {
+                                    Box {
+                                        IconButton(onClick = { navController.navigate(Screens.findNews.route) }, modifier = Modifier.padding(24.dp)) {
+                                            Icon(
+                                                Icons.Default.Search,
+                                                contentDescription = "Find News"
+                                            )
+                                        }
+                                    }
+                                    Box {
+                                        IconButton(
+                                            onClick = { navController.navigate(Screens.homeScreen.route) },
+                                            modifier = Modifier.padding(24.dp)
+                                        ) {
+                                            Icon(
+                                                Icons.Filled.Home,
+                                                contentDescription = "Home"
+                                            )
+                                        }
+                                    }
+                                    Box {
+                                        IconButton(onClick = { navController.navigate(Screens.favoriteNews.route) }, modifier = Modifier.padding(24.dp)) {
+                                            Icon(
+                                                Icons.Filled.Favorite,
+                                                contentDescription = "Favorite News"
+                                            )
+                                        }
+                                    }
+                                }
+                            }
+                        )
+                    }
+                ) { innerPadding ->
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screens.homeScreen.route,
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        composable(route = Screens.homeScreen.route) {
+                            HomeScreen()
+                        }
+                        composable(route = Screens.findNews.route) {
+                            SearchNewsScreen()
+                        }
+                        composable(route = Screens.favoriteNews.route) {
+                            FavoriteNewsScreen()
+                        }
+                    }
+                }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NewslyTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/github/diego22rct/newsly/home/presentation/homeScreen.kt
+++ app/src/main/java/com/github/diego22rct/newsly/home/presentation/homeScreen.kt
@@ -1,2 +1,22 @@
 package com.github.diego22rct.newsly.home.presentation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeScreen() {
+    Scaffold { paddingValues ->
+        Column (
+            modifier = Modifier.fillMaxWidth().padding(paddingValues),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Home")
+        }
+    }
+}

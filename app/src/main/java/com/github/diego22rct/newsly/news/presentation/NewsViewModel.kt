bfcb13fd08d8fefae75c5b329341cf2f79b93aa6
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-10-02 11:58:20 -0500

    implement the viewmodel to fetch the news while the app start

--- app/src/main/java/com/github/diego22rct/newsly/news/presentation/NewsViewModel.kt
+++ app/src/main/java/com/github/diego22rct/newsly/news/presentation/NewsViewModel.kt
@@ -1,6 +1,27 @@
 package com.github.diego22rct.newsly.news.presentation
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.github.diego22rct.appsuperzound.list_album.data.repository.NewsRepository
+import com.github.diego22rct.newsly.common.Resource
+import com.github.diego22rct.newsly.news.domain.model.New
+import kotlinx.coroutines.launch
 
-class NewsViewModel: ViewModel() {
+class NewsViewModel(private val newsRepository: NewsRepository) : ViewModel() {
+
+    private val _newsList = MutableLiveData<Resource<List<New>>>()
+    val newsList: LiveData<Resource<List<New>>> get() = _newsList
+
+    init {
+        fetchNews()
+    }
+
+    fun fetchNews() {
+        viewModelScope.launch {
+            val result = newsRepository.getNews()
+            _newsList.value = result
+        }
+    }
 }
